f99b65932bc9a07fd5ee193f4614d8b5bf355cf6
Unknown <rudsonmartina@gmail.com>
2022-06-11 03:04:03 +0200

    Papi-Gelato
    1.2 Added allTaste Function

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,17 +1,34 @@
 #Papi-Gelato
 
+def allTaste(): 
+    global tasteLst
+    tasteLst = []   
+    for x in range(iceScoops):
+
+        taste = input(f"Welke smaak wilt u voor bolletje nummer {iceScoops}?\nA) Aardbei\nC) Chocolade\nM) Munt\nV) Vanille\n").upper()
+        if taste == "A":
+            tasteLst.append("Aardbei")
+        elif taste == "C":
+            tasteLst.append("Chocolade")
+        elif taste == "M":
+            tasteLst.append("Munt")
+        elif taste == "V":
+            tasteLst.append("Vanille")
+        else:
+            print("Sorry dat snap ik niet...")
+            allTaste()
 
 
 def step1():
     global iceScoops
     global doosje
     iceScoops = int(input("Hoeveel bolletjes wilt u?"))
-
+    allTaste()
     if iceScoops in range(1, 4):
         doosje = "hoorntje"
         step2()
     elif iceScoops in range(4, 8):
-        print(f"Dan krijgt u van mij een bakje met {iceScoops} bolletjes")
+        print(f"Dan krijgt u van mij een bakje met {iceScoops} bolletjes met de smaken\n{tasteLst}")
         doosje = 'bakje'
         step3()
     elif iceScoops in range(8, 50):
@@ -34,7 +51,7 @@ def step2():
     step3()
 
 def step3():
-    x = input(f"Hier is uw {doosje} met {iceScoops} bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
+    x = input(f"Hier is uw {doosje} met {iceScoops} bolletje(s) met de smaken\n{tasteLst}.\n Wilt u nog meer bestellen? (Y/N)").upper()
     if x == "Y":
         step1()
     elif x == "N":
