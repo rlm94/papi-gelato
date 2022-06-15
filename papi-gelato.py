1b4801e5715b0e7e129439243c37799b56b7aa29
Unknown <rudsonmartina@gmail.com>
2022-06-15 09:29:53 +0200

    Papi Gelato
    1.3.1 added receipt functionality

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,11 +1,17 @@
 #Papi-Gelato
+scoop = 1.10
+cones = 1.25
+trays = 0.75
+scoopQty = 0
+conesQty = 0
+traysQty = 0
 
 def allTaste(): 
     global tasteLst
     tasteLst = []   
-    for x in range(iceScoops):
+    for x in range(1, iceScoops+1):
 
-        taste = input(f"Welke smaak wilt u voor bolletje nummer {iceScoops}?\nA) Aardbei\nC) Chocolade\nM) Munt\nV) Vanille\n").upper()
+        taste = input(f"Welke smaak wilt u voor bolletje nummer {x}?\nA) Aardbei\nC) Chocolade\nM) Munt\nV) Vanille\n").upper()
         if taste == "A":
             tasteLst.append("Aardbei")
         elif taste == "C":
@@ -22,15 +28,22 @@ def allTaste():
 def step1():
     global iceScoops
     global doosje
+    global conesQty
+    global traysQty
+    global scoopQty
     print("Welkom bij Papi Gelato")
     iceScoops = int(input("Hoeveel bolletjes wilt u?"))
-    allTaste()
+    #if iceScoops != range(8, 50):
+        
+    if iceScoops < 8:
+        allTaste()
+        scoopQty += iceScoops
     if iceScoops in range(1, 4):
-        doosje = "hoorntje"
         step2()
     elif iceScoops in range(4, 8):
         print(f"Dan krijgt u van mij een bakje met {iceScoops} bolletjes met de smaken\n{tasteLst}")
         doosje = 'bakje'
+        traysQty += 1
         step3()
     elif iceScoops in range(8, 50):
         print("Sorry, zulke grote bakken hebben we niet")
@@ -40,12 +53,16 @@ def step1():
         step1()
 
 def step2():
+    global traysQty
+    global conesQty
     global doosje
     x = input(f"Wilt u deze {iceScoops} bolletje(s) in A) een hoorntje of B) een bakje?").upper()
     if x == "A":
         doosje = 'hoorntje'
+        conesQty += 1
     elif x == "B":
         doosje = 'bakje' 
+        traysQty += 1
     else:
         print("Sorry dat snap ik niet...")
         step2()
@@ -57,8 +74,21 @@ def step3():
         step1()
     elif x == "N":
         print("Bedankt en tot ziens!")
+        receipt()
     else:
         print("Sorry dat snap ik niet...")
         step3()
-    
+
+def receipt():
+    print("---------[\"Papi Gelato\"]---------\n")
+
+    print("Bolletjes     {} x €{:.2f}   = €{:.2f}".format(scoopQty, scoop, (scoopQty*scoop)))
+    if conesQty != 0:
+        print("Horrentjes    {} x €{:.2f}   = €{:.2f}".format(conesQty, cones, (conesQty*cones)))
+    if traysQty != 0:
+        print("Bakjes        {} x €{:.2f}   = €{:.2f}".format(traysQty, trays, (traysQty*trays)))
+    print("                            -------- +")
+    print("Totaal                     = €{:.2f}".format((scoopQty*scoop)+(conesQty*cones)+(traysQty*trays)))
+
+
 step1()
