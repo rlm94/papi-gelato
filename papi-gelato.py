37ba8cc7985204e1d979d743dbb49b7403b6b940
Unknown <rudsonmartina@gmail.com>
2022-06-16 09:40:27 +0200

    Papi-Gelato
    1.4.1 added topping functionality

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,17 +1,58 @@
 #Papi-Gelato
+
+order = True
 scoop = 1.10
 cones = 1.25
 trays = 0.75
+cream = 0.50
+sprinkles = 0.30
+caramel = 0.60
+ice_liter = 9.80
+ice_literQty = 0
+iceQty = 0
 scoopQty = 0
 conesQty = 0
 traysQty = 0
+creamQty = 0
+sprinklesQty = 0
+caramelQty = 0
+toppingsQty = 0
+toppingsCounter = 0
 
-def allTaste(): 
+
+def welcome():
+    print("Welkom bij Papi Gelato")
+
+
+def iceScoops():
+    global scoopQty, scoops
+    scoops = int(input("Hoeveel bolletjes wilt u?"))
+    scoopQty += scoops
+    return scoops
+
+
+def tray_or_cone():
+    global trayOrCone, conesQty, traysQty, caramel
+    trayOrCone = input(f"Wilt u deze {scoops} bolletje(s) in A) een hoorntje of B) een bakje?").upper()
+    if trayOrCone == "A":
+        trayOrCone = "hoorntje"
+        conesQty += 1
+    elif trayOrCone == "B":
+        trayOrCone = "bakje"
+        traysQty += 1    
+    return trayOrCone
+
+
+
+def allTaste(type,qty):
     global tasteLst
-    tasteLst = []   
-    for x in range(1, iceScoops+1):
+    global client
 
-        taste = input(f"Welke smaak wilt u voor bolletje nummer {x}?\nA) Aardbei\nC) Chocolade\nM) Munt\nV) Vanille\n").upper()
+    tasteLst = []
+
+
+    for i in range(1,qty+1):
+        taste = input(f"Welke smaak wilt u voor {type} nummer {i}?\nA) Aardbei\nC) Chocolade\nM) Munt\nV) Vanille\n").upper()#
         if taste == "A":
             tasteLst.append("Aardbei")
         elif taste == "C":
@@ -22,73 +63,104 @@ def allTaste():
             tasteLst.append("Vanille")
         else:
             print("Sorry dat snap ik niet...")
-            allTaste()
+    return tasteLst
 
-
-def step1():
-    global iceScoops
-    global doosje
-    global conesQty
-    global traysQty
-    global scoopQty
-    print("Welkom bij Papi Gelato")
-    iceScoops = int(input("Hoeveel bolletjes wilt u?"))
-    #if iceScoops != range(8, 50):
-        
-    if iceScoops < 8:
-        allTaste()
-        scoopQty += iceScoops
-    if iceScoops in range(1, 4):
-        step2()
-    elif iceScoops in range(4, 8):
-        print(f"Dan krijgt u van mij een bakje met {iceScoops} bolletjes met de smaken\n{tasteLst}")
-        doosje = 'bakje'
-        traysQty += 1
-        step3()
-    elif iceScoops in range(8, 50):
-        print("Sorry, zulke grote bakken hebben we niet")
-        step1()
-    else:
-        print("Sorry dat snap ik niet...")
-        step1()
-
-def step2():
-    global traysQty
-    global conesQty
-    global doosje
-    x = input(f"Wilt u deze {iceScoops} bolletje(s) in A) een hoorntje of B) een bakje?").upper()
-    if x == "A":
-        doosje = 'hoorntje'
-        conesQty += 1
-    elif x == "B":
-        doosje = 'bakje' 
-        traysQty += 1
-    else:
-        print("Sorry dat snap ik niet...")
-        step2()
-    step3()
-
-def step3():
-    x = input(f"Hier is uw {doosje} met {iceScoops} bolletje(s) met de smaken\n{tasteLst}.\n Wilt u nog meer bestellen? (Y/N)").upper()
-    if x == "Y":
-        step1()
-    elif x == "N":
-        print("Bedankt en tot ziens!")
-        receipt()
-    else:
-        print("Sorry dat snap ik niet...")
-        step3()
 
 def receipt():
-    print("---------[\"Papi Gelato\"]---------\n")
 
-    print("Bolletjes     {} x €{:.2f}   = €{:.2f}".format(scoopQty, scoop, (scoopQty*scoop)))
+    print("---------[\"Papi Gelato\"]---------\n")
+    if scoopQty != 0:
+        print("Bolletjes     {} x €{:.2f}   = €{:.2f}".format(scoopQty, scoop, (scoopQty*scoop)))
     if conesQty != 0:
         print("Horrentjes    {} x €{:.2f}   = €{:.2f}".format(conesQty, cones, (conesQty*cones)))
     if traysQty != 0:
         print("Bakjes        {} x €{:.2f}   = €{:.2f}".format(traysQty, trays, (traysQty*trays)))
+    if toppingsQty != 0:
+        print("Toppings      {} x €{:.2f}   = €{:.2f}".format(toppingsCounter, toppingsQty, (toppingsQty)))
+    if ice_literQty != 0:
+        print("Liter         {} x €{:.2f}   = €{:.2f}".format(ice_literQty, ice_liter, (ice_liter*ice_literQty)))
     print("                            -------- +")
-    print("Totaal                     = €{:.2f}".format((scoopQty*scoop)+(conesQty*cones)+(traysQty*trays)))
+    print("Totaal                    = €{:.2f}".format((scoopQty*scoop)+(conesQty*cones)+(traysQty*trays)+(toppingsQty)+(ice_liter*ice_literQty)))
+    if ice_literQty != 0:
+        print("BTW(9%)                   = €{:.2f}".format((ice_liter*ice_literQty/109*9)))
+def toppings():
+    global toppingsQty
+    global toppingsCounter
+    global caramel
+    global trayOrCone
+    toppingsInput = input("\n A) Geen\n B) Slagroom\n C) Sprinkels\n D) Caramel Saus\nWat voor topping wilt u:").upper()
+    if toppingsInput == "A":
+        print("Oke u gaat voor geen toppings.")
+    elif toppingsInput == "B":
+        toppingsQty += cream
+        toppingsCounter += 1       
+    elif toppingsInput == "C":
+        toppingsQty += sprinkles
+        toppingsCounter += 1
+    elif toppingsInput == "D":
+        toppingsQty += caramel
+        toppingsCounter += 1
+        if trayOrCone == "bakje":
+            toppingsQty += 0.30
+    else:
+        print("Sorry dat snap ik niet...")
 
+def typeClient():
+    global client
+    client = input("Bent u A) particulier of B) zakelijk?\n:").upper()
+    if client == "A":
+        client = "particulier"
+    elif client == "B":
+        client = "zakelijk"
+    else:
+        print("Sorry dat snap ik niet...")
+    return client
+        
 
-step1()
+while order == True:
+    welcome()
+    typeClient()
+    if client == "particulier":
+        type = "bolletje"
+        iceScoops()
+
+        if scoops in range(1, 4):
+            tray_or_cone()
+            toppings()
+            allTaste(type, scoops)
+            stop_or_go = input(f"Hier is uw {trayOrCone} met {scoops} bolletje(s) met de smaken\n{tasteLst}.\n Wilt u nog meer bestellen? (Y/N)").upper()#
+            if stop_or_go == "Y":
+                order = True
+            elif stop_or_go == "N":
+                receipt()
+                print("Bedankt en tot ziens!")
+                order = False
+
+        elif scoops in range(4, 8):
+            traysQty += 1
+            trayOrCone = "bakje"
+            toppings()
+            allTaste(type, scoops)
+            print(f"Dan krijgt u van mij een bakje met {scoops} bolletjes met de smaken\n{tasteLst}  ")#
+            stop_or_go = input("Wilt u nog meer bestellen? (Y/N)\n:").upper()
+            if stop_or_go == "Y":
+                order = True
+            elif stop_or_go == "N":
+                receipt()
+                print("Bedankt en tot ziens!")
+                order = False
+
+        elif scoops in range(8, 50):
+            print("Sorry, zulke grote bakken hebben we niet")
+
+        else:
+            print("Sorry, dat snap ik niet...")
+    elif client == "zakelijk":
+        type = "liter"
+        businessInput = int(input("Hoeveel liter ijs wilt u?"))
+        ice_literQty += businessInput
+        allTaste(type, businessInput)
+        receipt()
+        order = False
+    else:
+        print("Sorry, dat snap ik niet...")
